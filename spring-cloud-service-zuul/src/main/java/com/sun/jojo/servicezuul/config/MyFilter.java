bd52ac93c0512041193044da2c92a353ceee5647
sunjiamin <823282324@qq.com>
2018-05-14 15:42:52 +0800

    添加zuul  网关

--- spring-cloud-service-zuul/src/main/java/com/sun/jojo/servicezuul/config/MyFilter.java
+++ spring-cloud-service-zuul/src/main/java/com/sun/jojo/servicezuul/config/MyFilter.java
@@ -21,21 +21,42 @@ public class MyFilter  extends ZuulFilter{
 
     private static Logger log = LoggerFactory.getLogger(MyFilter.class);
 
+    /**
+     * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     * @return
+     */
     @Override
     public String filterType() {
         return "pre";
     }
 
+    /**
+     * filterOrder：过滤的顺序
+     * @return
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
 
+    /**
+     * shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+     * @return
+     */
     @Override
     public boolean shouldFilter() {
         return true;
     }
 
+    /**
+     * run：过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+     * @return
+     * @throws ZuulException
+     */
     @Override
     public Object run() throws ZuulException {
 
